8f75d66da28017774347f655b443b02d36237970
Steve Karg <steve@kargs.net>
2013-05-09 17:05:56 -0500

    Create factors.c

---
+++ factors.c
@@ -0,0 +1,48 @@
+/*
+* Copyright (c) 2004 Stephen Karg
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  long index = 0;
+  long factor = 0;
+  long integer = 0;
+
+  if (argc < 2)
+  {
+    printf("%s <integer> - finds all the whole factors of a number\r\n",argv[0]);
+    return 0;
+  }
+
+  integer = strtol(argv[1],NULL,0);
+  printf("factors of %ld:\n",integer);
+  for (index = 1; index <= integer; index++)
+  {
+    factor = integer / index;
+    if (!(integer % index))
+      printf("%ld x %ld\n",index,factor);
+  }
+
+  return 1;
+}
